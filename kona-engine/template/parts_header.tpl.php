af71d9b2ea8d0ad666f1f64cd84e9c2992e93c23
kujirahand <web@kujirahan.com>
2013-11-23 17:59:56 +0800

    og:urlでプロトコルが欠けていたので追加

--- kona-engine/template/parts_header.tpl.php
+++ kona-engine/template/parts_header.tpl.php
@@ -3,8 +3,8 @@
 // HTML COMMON HEADER FILE
 //----------------------------------------------------------------------
 $log_id     = konawiki_getPageId();
-$baseuri    = "//".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
-$shorturi   = "//".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']
+$baseuri    = "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+$shorturi   = "http://".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']
               . "?{$log_id}&amp;go"; 
 // check skin & theme
 $skin     = konawiki_public("skin", "default");
