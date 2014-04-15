d45dd59142da248d3b0d3fb9077678253f64973c
kujirahand <web@kujirahan.com>
2014-04-15 10:47:30 +0800

    add socialbutton plugin, fixed date bug in blogtop plugin

---
+++ kona-engine/plugins/socialbutton.inc.php
@@ -0,0 +1,14 @@
+<?php
+/** konawiki plugins -- Twitter/はてブ/Facebook/Google+のソーシャルボタンを表示する 
+ * - [書式] #socialbutton
+ * - [引数]
+ * -- 文字列
+ * - [使用例] #md5(あいうえお)
+ * - [備考] なし
+ */
+
+function plugin_socialbutton_convert($params)
+{
+  
+}
+
