2b1c92c98c75278170b6d547685ed022a731dd9a
kujirahand.com <web@kujirahand.com>
2015-02-27 13:43:52 +0900

    fixed UserAgent when enpty

--- kona-engine/lib/useragent.inc.php
+++ kona-engine/lib/useragent.inc.php
@@ -6,18 +6,18 @@
  */
 function kona_getUserAgent()
 {
-	$ua = $_SERVER["HTTP_USER_AGENT"];
-	return $ua;
+  $ua = isset($_SERVER["HTTP_USER_AGENT"]) ? $_SERVER["HTTP_USER_AGENT"] : '';
+  return $ua;
 }
 
 function useragent_is_galapagos()
 {
-	return useragent_is_docomo() || useragent_is_softbank() || useragent_is_au();
+  return useragent_is_docomo() || useragent_is_softbank() || useragent_is_au();
 }
 
 function useragent_is_smartphone()
 {
-	return 
+  return 
     konawiki_public('mobile', false) ||
     useragent_is_iPhone() || 
     useragent_is_android();
@@ -25,38 +25,38 @@ function useragent_is_smartphone()
 
 function useragent_is_iPad()
 {
-	$ua = $_SERVER["HTTP_USER_AGENT"];
- 	return (strpos($ua, "iPad") >= 1);
+  $ua = kona_getUserAgent();
+  return (strpos($ua, "iPad") >= 1);
 }
 
 function useragent_is_iPhone()
 {
-	$ua = $_SERVER["HTTP_USER_AGENT"];
- 	return (strpos($ua, "iPhone") >= 1) || (strpos($ua, "iPod") >= 1);
+  $ua = kona_getUserAgent();
+  return (strpos($ua, "iPhone") >= 1) || (strpos($ua, "iPod") >= 1);
 }
 
 function useragent_is_android()
 {
-	$ua = $_SERVER["HTTP_USER_AGENT"];
- 	return (strpos($ua, "Android") >= 1);
+  $ua = kona_getUserAgent();
+  return (strpos($ua, "Android") >= 1);
 }
 
 function useragent_is_docomo()
 {
-	$agent = $_SERVER['HTTP_USER_AGENT']; 
-	return (ereg("^DoCoMo", $agent));
+  $ua = kona_getUserAgent();
+  return (ereg("^DoCoMo", $agent));
 }
 
 function useragent_is_softbank()
 {
-	$agent = $_SERVER['HTTP_USER_AGENT']; 
-	return (ereg("^J-PHONE|^Vodafone|^SoftBank", $agent));
+  $ua = kona_getUserAgent();
+  return (ereg("^J-PHONE|^Vodafone|^SoftBank", $agent));
 }
 
 function useragent_is_au()
 {
-	$agent = $_SERVER['HTTP_USER_AGENT']; 
-	return (ereg("^UP.Browser|^KDDI", $agent));
+  $ua = kona_getUserAgent();
+  return (ereg("^UP.Browser|^KDDI", $agent));
 }
 
 
