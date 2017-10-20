6300e1d9b583ce5f36bd0e2688846402b244d7c2
kujirahand <web@kujirahand.com>
2017-10-21 00:00:21 +0900

    文字の装飾コードを追加''str''/``str``/%%str%%

--- kona-engine/lib/konawiki_parser.inc.php
+++ kona-engine/lib/konawiki_parser.inc.php
@@ -508,6 +508,20 @@ function __konawiki_parser_tohtml(&$text, $level)
             $result .= "<strong>$str</strong>";
             continue;
         }
+        if ($c2 == "``") {
+            $text = substr($text, 2);
+            $s = konawiki_parser_token($text, "``");
+            $str = konawiki_parser_tohtml($s);
+            $result .= "<span class='marker'>$str</span>";
+            continue;
+        }
+        if ($c2 == '%%') {
+            $text = substr($text, 2);
+            $s = konawiki_parser_token($text, "%%");
+            $str = konawiki_parser_tohtml($s);
+            $result .= "<span class='red'>$str</span>";
+            continue;
+        }
         // url
         if (preg_match('@^(http|https|ftp)\://[\w\d\.\#\$\%\&\(\)\-\=\_\~\^\|\,\.\/\?\+\!\[\]\@]+@', $text, $m) > 0) {
             $result .= konawiki_parser_makeUriLink($m[0]);
