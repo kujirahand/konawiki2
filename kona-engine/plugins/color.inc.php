54b8b42a2913d80d7783a0cc36cdf41cbf8ab14c
kujirahand <kujira@kujirahand.com>
2021-02-20 22:19:22 +0900

    color pluginのxssを修正:

--- kona-engine/plugins/color.inc.php
+++ kona-engine/plugins/color.inc.php
@@ -19,7 +19,9 @@ function plugin_color_convert($params)
 	
     $color = array_shift($params);
     $s = join(",",$params);
+
     $s = konawiki_parser_tohtml(trim($s));
+    $color = konawiki_parser_tohtml(trim($color));
     return <<<EOS__
 <span style="color:$color;">$s</span>
 EOS__;
