b2a5adb9cfa635fb0621e6cbf0b2fb114eec3290
kujirahand <web@kujirahand.com>
2024-02-15 11:09:55 +0900

    add clear plugin

---
+++ kona-engine/plugins/clear.inc.php
@@ -0,0 +1,14 @@
+<?php
+/** konawiki plugins -- 回り込みを解除するプラグイン
+ * - [書式]
+ * #clear
+ * - [引数]
+ * - [使用例] #clear(コメント);
+ * - [備考]
+ * - [公開設定] 公開
+ */
+
+function plugin_clear_action($params)
+{
+    return "<div style='clear:both;'></div>";
+}
